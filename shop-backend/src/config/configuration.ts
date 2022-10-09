984718ba3b9222d642d62b73f5db85bb1d90cbdd
jon-pruessmeier <pruessmeierjon@gmail.com>
2022-10-09 19:32:18 +0200

    configs

--- shop-backend/src/config/configuration.ts
+++ shop-backend/src/config/configuration.ts
@@ -3,7 +3,7 @@ export default () => ({
     type: process.env.DB_TYPE,
     host: process.env.DB_HOST,
     // @ts-ignore
-    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    port: parseInt(process.env.DB_PORT, 10) || 5433,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE,
