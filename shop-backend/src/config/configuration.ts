677a66dd5fc15e02f04cc0b12859727c174c9cbb
jon-pruessmeier <pruessmeierjon@gmail.com>
2022-10-10 09:47:28 +0200

    changed configs for local database instead of docker db

--- shop-backend/src/config/configuration.ts
+++ shop-backend/src/config/configuration.ts
@@ -1,9 +1,9 @@
 export default () => ({
   database: {
-    type: process.env.DB_TYPE,
+    type: 'postgres',
     host: process.env.DB_HOST,
     // @ts-ignore
-    port: parseInt(process.env.DB_PORT, 10) || 5433,
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE,
